1963bd2711c054ce77d648b09a3fc8e9fa45c9d8
K1llM@n <k1llmanws@yandex.ru>
2021-11-30 00:08:08 +0300

    Test command

---
+++ GtaModdingCli/Commands/Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+
+using GtaModdingCli.Common;
+
+using Sharprompt;
+
+using UAssetAPI;
+using UAssetAPI.UnrealTypes.Enums;
+
+namespace GtaModdingCli.Commands
+{
+    [CliCommand("Test asset.",
+        "Usage: test [asset]\n" +
+        "asset: Path to asset.",
+        "test"
+    )]
+    public class Test : AbstractCliCommand
+    {
+        public override void Execute(string[] args)
+        {
+            UAsset asset = new(args[0], UE4Version.VER_UE4_26);
+        }
+
+        public override string[] GetInteractiveData()
+        {
+            return new[] { 
+                Prompt.Input<string>("Path to asset", validators: new List<Func<object, ValidationResult>> { FileExists })
+            };
+        }
+
+        public Test(Cli cli) : base(cli)
+        {
+
+        }
+    }
+}
